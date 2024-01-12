43a16f0e1013cd587ef9cf7c0c41742350866215
Akashw24 <134467212+Akashw24@users.noreply.github.com>
2024-01-12 15:56:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,39 @@
+package Array.arrayeg;
+
+//*Write a program to accept 10 elements from user and find the largest element and smallest element using 1D Array.
+//import java.util.Scanner;
+
+
+//*USE OF ENHANCED FOR LOOPS
+//*Enhanced for loops are used to iterate over arrays and collections.
+
+class Array {
+    public static void main(String[] args) {
+        int[] my_array = {10, 20, 30, 40, 50};
+        for (int element : my_array) {
+            System.out.println("The elements of an array arr " + element);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
